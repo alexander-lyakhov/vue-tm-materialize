514d08ae1758d6db7cc553a4be64be90594cec29
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2020-06-26 16:37:06 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'materialize-css/dist/js/materialize.min';
+import 'materialize-css/dist/css/materialize.min.css';
+
 Vue.config.productionTip = false
 
 new Vue({
